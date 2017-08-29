c38e38263b50d5edf8ea6ec6c8fea0365ce94075
Chris <chris@usablehq.com>
2017-08-29 20:35:14 +0100

    Added caching fetch:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const baseURL = 'https://api.github.com';
+//axios.defaults.headers.common['Accept'] = 'application/vnd.github.v3+json';
+//axios.defaults.headers.common['User-Agent'] = 'onion2k-garden';
+//axios.defaults.headers.common['Authorization'] = 'token ';
+
+function cacheFetch(url) {
+
+  var qualifiedUrl = baseURL+url;
+
+  const cached = localStorage.getItem(qualifiedUrl);
+  if (cached) {
+    return Promise.resolve(JSON.parse(cached));
+  }
+
+  return fetch(qualifiedUrl).then((response)=>{
+    return response.json();
+  }).then((response)=>{
+    localStorage.setItem(qualifiedUrl, JSON.stringify(response));
+    return response;
+  });
+
+}
+
 class App extends Component {
+
+  componentWillMount(){
+
+    cacheFetch('/users/onion2k/repos')
+      .then((result)=>{
+        console.log(result);
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+
+    cacheFetch('/repos/onion2k/soundboard/commits')
+      .then((result)=>{
+        console.log(result);
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+    
+  }
   render() {
     return (
       <div className="App">
